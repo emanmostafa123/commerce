48691c4c7622ea94433abcfbc0639ab4a0580243
eman mostafa <mostafaeman787@gmail.com>
2024-12-12 18:54:29 +0200

    upd

---
+++ src/app/shared/authguard.guard.ts
@@ -0,0 +1,29 @@
+// auth-guard.service.ts
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  
+  isAuthenticated: boolean;
+  constructor(
+    public router : Router
+  ){
+    this.isAuthenticated = false;
+    if(localStorage.getItem("token") != null) this.isAuthenticated = true
+    
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.isAuthenticated) {
+      return true;
+    } else {
+      this.router.navigate(['/login']); // Redirect to login if not authenticated
+      return false;
+    }
+    }
+}
